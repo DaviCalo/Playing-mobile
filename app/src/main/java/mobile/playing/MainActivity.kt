9d98de7e90f76aa5631c127a12ac6ad035fa54e6
Davi C. Pes. <davinogueiraen@email.com>
2024-03-06 21:12:19 -0300

    teste de botao

--- app/src/main/java/mobile/playing/MainActivity.kt
+++ app/src/main/java/mobile/playing/MainActivity.kt
@@ -1,11 +1,25 @@
 package mobile.playing
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import mobile.playing.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.buttonEnter.setOnClickListener(this)
     }
+
+    override fun onClick(v: View) {
+        val intentE = Intent(this, MainActivity2::class.java)
+        startActivity(intentE)
+    }
+
+
 }
